97653c7f0d04c8e7606d4dd27483e158a6228b9e
aymanessobhi <ayman.essobhi@gmail.com>
2022-08-15 21:56:44 +0000

    Create Ex1.cpp

---
+++ pointers/Ex1.cpp
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(){
+float a,b,*p,*pt;
+p=&a;
+pt=&b;
+
+printf("Entrer un nombre :");scanf("%f",p);
+printf("Entrer un nombre :");scanf("%f",pt);
+//calcule la somme
+printf("/nla somme est :%.2f\n",*p + *pt);
+//calcule le produit
+printf("le produit est :%.2f\n",*p * *pt);
+//la difference
+printf("la difference est :%.2f\n",*p - *pt);
+//devision de deux nombre rell
+	if(*p!=0)
+		printf("la devision est :%.2f\n",*p / *pt);
+	else{
+		printf("Impossible de deviser par 0 !!!");
+	}
+
+}
