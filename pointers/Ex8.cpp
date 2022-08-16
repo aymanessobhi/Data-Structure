bcd8068ead1bdaaaa52fcdf65affbe21acb4d6f5
aymanessobhi <ayman.essobhi@gmail.com>
2022-08-16 17:15:34 +0000

    Create Ex8.cpp

---
+++ pointers/Ex8.cpp
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){
+	
+int a[50],b[50],m,n,*pa,*pb;
+pa=a; //pa=&a[0];
+pb=b; //pb=&b[0];
+ 
+printf("Entrer la dimension de tableau a :");scanf("%d",&m);
+printf("Entrer la dimension de tableau b :");scanf("%d",&n);
+printf("Entrer les elements de tableau a :\n");
+for(;pa<a+m;pa++){
+	scanf("%d",pa);
+}
+printf("Entrer les elements de tableau b :\n");
+for(;pb<b+n;pb++){
+	scanf("%d",pb);
+}
+for(pa=a+m,pb=b;pa<a+m+n;pa++,pb++){	
+	*pa=*pb;
+}
+for(pa=a;pa<a+n+m;pa++){
+	printf(" %d ",*pa);
+}
+}
+
